8ea59434929c0c4f2db863e468fc6c9decb0b8ad
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-03 02:25:54 +0000

    COMPLETED 1.3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,28 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class PracticeProblem {
+    public static String readFile(String filename) {
+        try {
+            java.nio.file.Path path = java.nio.file.Paths.get(filename);
+            return new String(java.nio.file.Files.readAllBytes(path));
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+            return ""; 
+        }
+    }
 
-	public static void main(String args[]) {
 
-	}
 
-	
-
+public static String backwardsReadFile(String filename) {
+    try {
+        String content = new String(Files.readAllBytes(Paths.get(filename)));
+        return new StringBuilder(content).reverse().toString();
+    } catch (IOException e) {
+        e.printStackTrace();
+        return ""; 
+    }
+}
 }
